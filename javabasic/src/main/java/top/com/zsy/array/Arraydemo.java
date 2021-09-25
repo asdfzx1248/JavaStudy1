10dbf4e448728e21d6292c301ab04d2d5e324258
asdfzx1248 <90694887+asdfzx1248@users.noreply.github.com>
2021-09-25 22:21:54 +0800

    第一次修改

--- javabasic/src/main/java/top/com/zsy/array/Arraydemo.java
+++ javabasic/src/main/java/top/com/zsy/array/Arraydemo.java
@@ -1,4 +1,11 @@
 package top.com.zsy.array;
 
 public class Arraydemo {
+    public int getSum(int[] array){
+        int sum = 0;
+        for(int i=0;i<array.length;i++){
+            sum +=i;
+        }
+        return sum;
+    }
 }
