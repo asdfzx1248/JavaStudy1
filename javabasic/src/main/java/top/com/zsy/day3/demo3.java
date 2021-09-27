89f2f11ce37932964c85c8a1941e000525c2e28d
asdfzx1248 <90694887+asdfzx1248@users.noreply.github.com>
2021-09-27 09:53:46 +0800

    docs:添加了9月27号上课的文档

---
+++ javabasic/src/main/java/top/com/zsy/day3/demo3.java
@@ -0,0 +1,28 @@
+package top.com.zsy.day3;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class demo3 {
+    public static void main(String[] args) {
+        Integer a = 127;
+        Integer b = 127;
+        log.info("a == b ? {}" ,a == b);
+
+        Integer c = 128;
+        Integer d = 128;
+        log.info("c == d ? {}" ,c == d);
+
+        Integer e = 127;
+        Integer f = new Integer(127);
+        log.info("e == f ? {}" ,e == f);
+
+        Integer g = new Integer(127);
+        Integer h = new Integer(127);
+        log.info("g == h ? {}" ,g == h);
+
+        Integer i = 128;
+        int j = 128;
+        log.info("i == j ? {}", i == j);
+    }
+}
